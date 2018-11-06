d564bbc34b31764ce3c588fd0389d5367c14f675
je-an <mail@jeremiasandris.de>
2018-11-06 14:57:15 +0100

    Methods for selection and deselection are added

--- src/Button.js
+++ src/Button.js
@@ -21,8 +21,8 @@ define([ // jscs:ignore
          * @param {Object} options - options object
          * @param {String} options.id - id of the button
          * @param {String} options.name - name of the button
+         * @param {Boolean} options.isSelected - True if button shall be selected, false otherwise
          * @param {Function} options.onButtonClick - Gets called, if button is clicked
-         * @param {Boolean} [options.isHighlighted=false] - True if button stays highlighted after a click, false otherwise
          */
         var Button = function (options) {
             Inheritance.inheritConstructor(DomElement, this, Merge({ // jscs:ignore
@@ -30,12 +30,16 @@ define([ // jscs:ignore
                 name: TypeCheck.isString(options.name) ? options.name : Failure.throwTypeError("options.name is not a string"),
                 html: '<div id="' + options.id + '" class="jean-button">' + options.name + '</div>',
                 onButtonClick: TypeCheck.isString(options.heading) ? options.heading : "Collapsible",
-                isHighlighted: TypeCheck.isBoolean(options.isHighlighted) ? options.isHighlighted : false,
-                isSelected: false
+                isSelected: TypeCheck.isBoolean(options.isSelected) ? options.isSelected : false,
             }, TypeCheck.isDefined(options) ? options : {}));
             this.element.addEventListener("click", this._onButtonClick.bind(this));
             this.id = this.options.id;
             this._setState();
+        };
+        /** @enum */
+        Button.selectType = Button.prototype.selectType = {
+            SELECTED: "selected",
+            DESELECTED: "deselected"
         };
         /** */
         Button.prototype._onButtonClick = function () {
@@ -46,13 +50,21 @@ define([ // jscs:ignore
         /** */
         Button.prototype._setState = function () {
             var options = this.options;
-            if (options.isHighlighted) {
-                if (options.isSelected) {
-                    this.element.classList.add("jean-button-selected");
-                } else {
-                    this.element.classList.remove("jean-button-selected");
-                }
+            if (options.isSelected) {
+                this.element.classList.add("jean-button-selected");
+            } else {
+                this.element.classList.remove("jean-button-selected");
             }
+        };
+        /** */
+        Button.prototype.select = function () {
+            this.options.isSelected = true;
+            this._setState();
+        };
+        /** */
+        Button.prototype.deselect = function () {
+            this.options.isSelected = false;
+            this._setState();
         };
         return Button;
     });
